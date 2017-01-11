57c65da925e8647a6b999e1159254d4cdb9ffe9a
Raja Jawahar <rajajawahar77@gmail.com>
2017-01-11 15:46:14 +0530

    Initial Commit

---
+++ GoogleMapiOSSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapiOSSample
+//
+//  Created by rajamohamed on 11/01/17.
+//  Copyright © 2017 sedintechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
